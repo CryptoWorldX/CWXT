59e09c1af0c2a0713d4eba4a70ab315a24c43d4e
CryptoWorldX <git@git>
2016-12-24 20:53:25 +0100

    First Release

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define cryptoworldxtoken-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "cryptoworldxtokenURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
